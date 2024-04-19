85b75d3a8d01173ebceb084ccdf0714cab94bc84
Giorgos Mathioudakis <92247285+GiorgosMathioudakis@users.noreply.github.com>
2024-04-19 15:52:55 +0300

    Update Product.java

--- src/Models/Product.java
+++ src/Models/Product.java
@@ -1,2 +1,75 @@
-package Models;public class Product {
+package Models;
+
+public class Product {
+    private String SKU;
+    private String description;
+    private double salesPrice;
+    private double purchasePrice;
+
+    public Product(String SKU, String description, double salesPrice, double purchasePrice) {
+        this.SKU = SKU;
+        this.description = description;
+        this.salesPrice = salesPrice;
+        this.purchasePrice = purchasePrice;
+
+        System.out.println("Created Product: " + this);
+    }
+
+    @Override
+    public String toString() {
+        return "SKU='" + SKU + '\'' +
+                ", description='" + description + '\'' +
+                ", salesPrice=" + salesPrice +
+                ", purchasePrice=" + purchasePrice;
+    }
+
+    public String toJson(){
+        String productJson =  "{" +
+                "  \"mvProduct\": {" +
+                "    \"ProductType\": \"ProductType\"," +
+                "    \"ProductSKU\":" + getSKU() + "," +
+                "    \"ProductDescription\":" + getDescription() + "," +
+                "    \"ProductSellingPrice\":" + getSalesPrice() + "," +
+                "    \"ProductPurchasePrice\":" + getPurchasePrice() + "," +
+                "  }," +
+                "  \"mvRecordAction\": \"Insert\"," +
+                "  \"mvInsertUpdateDeleteSourceApplication\": \"myself\"" +
+                "}";
+
+        return productJson;
+    }
+
+
+    // Getters and setters
+    public String getSKU() {
+        return SKU;
+    }
+
+    public void setSKU(String SKU) {
+        this.SKU = SKU;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getSalesPrice() {
+        return salesPrice;
+    }
+
+    public void setSalesPrice(double salesPrice) {
+        this.salesPrice = salesPrice;
+    }
+
+    public double getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(double purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
 }
