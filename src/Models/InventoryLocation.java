3674153156a7fe32b71d9328c8428d30a27f8d49
Giorgos Mathioudakis <92247285+GiorgosMathioudakis@users.noreply.github.com>
2024-04-19 15:52:40 +0300

    Update InventoryLocation.java

--- src/Models/InventoryLocation.java
+++ src/Models/InventoryLocation.java
@@ -1,2 +1,65 @@
-package Models;public class InventoryLocation {
+package Models;
+
+public class InventoryLocation {
+    private String abbreviation ;
+    private String name;
+    private String address;
+
+    //Constructor
+    public InventoryLocation(String abbreviation , String name , String address){
+        this.abbreviation = abbreviation;
+        this.name = name;
+        this.address = address;
+
+        System.out.println("Created InventoryLocation: " + this);
+    }
+
+    @Override
+    public String toString() {
+        return "InventoryLocation{" +
+                "abbreviation='" + abbreviation + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    // Getters and setters
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+
+    public void setAbbreviation(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String toJson() {
+        // Convert the Product object to a JSON string for API use
+        String productJson =  "{\n" +
+                "  \"mvInventoryLocation\": {\n" +
+                "    \"InventoryLocationName\":" + getName() + ",\n" +
+                "    \"InventoryLocationAbbreviation\":" + getAbbreviation() + "\n" +
+                "  },\n" +
+                "  \"mvRecordAction\": \"Insert\",\n" +
+                "  \"mvInsertUpdateDeleteSourceApplication\": \"MyCompany\"\n" +
+                "}";
+
+        return productJson;
+    }
 }
+
